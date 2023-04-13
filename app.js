089dd94727f434336b171dffa3ea2a56bccc666f
sageer93 <128065898+sageer93@users.noreply.github.com>
2023-04-13 12:30:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ require("dotenv").config()
 DB_Con()
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(cors())
+app.use(cors({
+        origin:"https://my-m7ao.onrender.com",
+        credentials:true,
+        methods:['POST']
+    }))
 app.use("/api",router)
 app.use(express.static(path.join(__dirname,"./client/build")))
 app.get("*",function(req,res){
